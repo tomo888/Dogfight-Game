6efb0d401a4693e32f3de118204c39b57e65c8f2
Tom <colaboy98@gmail.com>
2019-01-03 01:25:05 +0000

    Version 2
    Created fire gun script; created bullet garbage collection script; created delayed box collider enable script.

---
+++ Assets/Scripts/BulletSelfDestruct.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class BulletSelfDestruct : MonoBehaviour
+{
+    /**Garbage collection for bullets; saves
+    excessive amounts of memory being used**/
+
+    public float timeUntilDestruct;
+
+    void Start()
+    {
+        Invoke("selfDestruct", timeUntilDestruct);
+    }
+
+    void selfDestruct()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/DelayedColliderEnable.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class DelayedColliderEnable : MonoBehaviour
+{
+    /** Script delays when the box collider enables; necessary
+    so that bullet doesn't hit player's plane when fired**/
+
+    public float afterSeconds;
+
+    void Start()
+    {
+        Invoke("Trigger", afterSeconds);
+    }
+
+    // Update is called once per frame
+    void Trigger()
+    {
+        transform.GetComponent<BoxCollider>().enabled = true;
+    }
+}
