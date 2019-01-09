b90c0e27b3b67ed3d30629ce8554525f39ada12e
Tom <colaboy98@gmail.com>
2019-01-09 02:04:40 +0000

    Version 4
    Prototype main menu UI added; various bug fixes for game.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Level Select");
+    }
+    public void Tutorial()
+    {
+        SceneManager.LoadScene("Tutorial");
+    }
+    public void Tutorial1()
+    {
+        SceneManager.LoadScene("Tutorial 1");
+    }
+    public void Tutorial2()
+    {
+        SceneManager.LoadScene("Tutorial 2");
+    }
+    public void EndGame()
+    {
+        SceneManager.LoadScene("Game Over");
+    }
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
